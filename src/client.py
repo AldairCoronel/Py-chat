49ba2abb64cbb6f8ab38daa1f48fd529a790bf4f
AldairCoronel <aldaircr@ciencias.unam.mx>
2018-09-19 15:23:10 -0500

    Server file improvement Added new featured in the file

---
+++ src/client.py
@@ -0,0 +1,34 @@
+import sys, socket, threading
+
+class Client:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self, HOST, PORT):
+        self.server.connect((HOST, PORT))
+        clientThread = threading.Thread(target = self.send_message)
+        clientThread.daemon = True
+        clientThread.start()
+        while True:
+            message = self.server.recv(1024)
+            if not message:
+                break
+            print(message)
+
+    def send_message(self):
+        while True:
+            self.server.send(bytes(input(""), 'UTF-8'))
+
+
+
+
+
+
+if len(sys.argv) != 3:
+    print ("Error: necesitas pasar: archivo, IP, puerto.")
+    exit()
+if(str(sys.argv[1] == 'localhost')):
+    HOST = '127.0.0.1'
+else:
+    HOST = str(sys.argv[1])
+PORT = int(sys.argv[2])
+client = Client(HOST, PORT)
--- src/server.py
+++ src/server.py
@@ -1,34 +1,69 @@
-import sys, socket
-from tread import *
+import sys, socket, threading
 
 class Server:
     clients = []
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    aceptarClientes = False
     def __init__(self, HOST, PORT):
         self.server.bind((HOST, PORT))
         self.server.listen(10000)
         self.clients = []
+        self.aceptarClientes = True
 
-#    def handler(self, connection, address):
-
-
-    def accept_new_client(self):
+    def recieve_message(self, connection, address, clients):
         while True:
+            message = connection.recv(1024)
+            message = message.decode("utf-8")
+            args = []
+            message = message.split(" ")
+            if message[0] == "USERS":
+                listClients = ""
+                for client in self.clients:
+                    listClients += str(client)
+                for client in self.clients:
+                    client.send(bytes(listClients, "UTF-8"))
+            elif message[0] == "PUBLICMESSAGE":
+                variable = ""
+                for i in range(1, len(message)):
+                    variable += message[i] + " "
+                for client in self.clients:
+                    client.send(bytes(variable, "UTF-8"))
+            if not message:
+                break
+
+        # while True:
+        #     message = connection.recv(1024)
+        #     protocolo = message.decode("utf-8")
+        #     if protocolo == "USERS":
+        #         listClients = ""
+        #         for client in self.clients:
+        #             listClients += str(client)
+        #         for client in self.clients:
+        #             client.send(bytes(listClients, "UTF-8"))
+        #     if not message:
+        #         break
+
+    def arriba(self):
+        while self.aceptarClientes:
                 connection, address = self.server.accept()
-                name = connection.server.recv(1024)
-                print("%s acaba de conectarse" %name)
+                # name = connection.server.recv(1024)
+                print("acaba de conectarse")
                 self.clients.append(connection)
-                clientThread = threading.Thread(target = self.handler, args=(Co, PORT))
-                clientThread.daemon = True
-                clientThread.start()
-                print(self. clients)
+                serverThread = threading.Thread(target = self.recieve_message,
+                                args=(connection, address, self.clients))
+                # serverThread.daemon = True
+                serverThread.start()
+                print(self.clients)
 
 
-
-if len(sys.argv) != 3
-    print "Error: necesitas pasar: archivo, IP, puerto."
+#
+if len(sys.argv) != 3:
+    print ("Error: necesitas pasar: archivo, IP, puerto.")
     exit()
-HOST = str(sys.argv[1])
-PORT = str(sys.argv[2])
+if(str(sys.argv[1] == 'localhost')):
+    HOST = '127.0.0.1'
+else:
+    HOST = str(sys.argv[1])
+PORT = int(sys.argv[2])
 server = Server(HOST, PORT)
-server.run
+server.arriba()
