a694528b046c9450041e42af6dcd7fe2b8337c45
AldairCoronel <aldaircr@ciencias.unam.mx>
2018-09-20 14:55:21 -0500

    PROTOCOL: MESSAGE ADDED
    Public message is now available

--- src/server.py
+++ src/server.py
@@ -31,18 +31,37 @@ class Server:
             client.get_socket().send(bytes('Nombre repetido', 'UTF-8'))
 
 
-    def send_clients(self):
+    def send_clients(self   ):
         listClients = ''
         for client in self.clients:
-            listClients += str(client)
+            listClients += str(client) + ', '
         for client in self.clients:
             client.get_socket().send(bytes(listClients, 'UTF-8'))
 
+    def verify_user_existance(self, user):
+        for client in self.clients:
+            if user == client.get_name():
+                return True
+        return False
 
-    def send_message(self, userMessage):
+
+    def get_user_socket(self, user):
+        for client in self.clients:
+            if user == client.get_name():
+                return client.get_socket()
+
+
+    def send_public_message(self, userMessage):
         for client in self.clients:
             client.get_socket().send(bytes(userMessage, 'UTF-8'))
 
+
+    def send_direct_message(self, user, message, client):
+        if self.verify_user_existance(user):
+            destination = self.get_user_socket(user)
+            destination.send(bytes(message, 'UTF-8'))
+        else:
+            client.get_socket().send(bytes('Usuario no encontrado', 'UTF-8'))
 
     def verify_status(self, status, client):
         verified = True
@@ -65,6 +84,12 @@ class Server:
             client.get_socket().send(bytes('Estado actualizado exitosamente',
                                             'UTF-8'))
 
+    def get_user_message(self, message, indice):
+        userMessage = ''
+        for i in range(indice, len(message)):
+            userMessage += message[i] + ' '
+        return userMessage
+
 
     def receive_message(self, client):
         while True:
@@ -76,18 +101,19 @@ class Server:
                 self.change_client_name(message[1], client)
 
             elif message[0] == Protocol.STATUS.value and len(message) > 1:
-                self.change_user_status(message[1],client)
+                self.change_user_status(message[1], client)
 
-            # elif message[0] == Protocol.MESSAGE.value:
+            elif message[0] == Protocol.MESSAGE.value and len(message) > 2:
+                message_to_user = message[1]
+                userMessage = self.get_user_message(message, 2)
+                self.send_direct_message(message_to_user, userMessage, client)
 
             elif message[0] == Protocol.USERS.value:
                 self.send_clients()
 
             elif message[0] == Protocol.PUBLICMESSAGE.value:
-                userMessage = ''
-                for i in range(1, len(message)):
-                    userMessage += message[i] + ' '
-                self.send_message(userMessage)
+                userMessage = self.get_user_message(message, 1)
+                self.send_public_message(userMessage)
 
             # elif message[0] == Protocol.CREATEROOM.value:
             #
